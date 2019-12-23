acb6235974e883dbd78314d05299dd950cd16b09
= <zainr7989@gmail.com>
2019-12-23 10:59:13 -0500

    [FIX] Bugs and documentation.

--- anagrams.java
+++ anagrams.java
@@ -1,5 +1,5 @@
 /* The purpose of this Hacker Rank challenge is to write a method that,
- * given two Strings, determine if those Strings are anagrams.
+ * given two Strings, can determine if those Strings are anagrams.
  * An anagram is any word which has all the same letters as another word, in the
  * same amounts as the other word, but may
  * or may not have them spelled the same way.
@@ -9,7 +9,8 @@
   * check out https://www.hackerrank.com/challenges/java-anagrams/problem?isFullScreen=true
   */
 
-  /* Credit for this starter code belongs to Hacker Rank, at
+  /* Credit for the starter code, including the main method and the declaration
+  * of the isAnagram method, belongs to Hacker Rank, at
    * https://www.hackerrank.com/challenges/java-anagrams/problem?isFullScreen=true
    */
 
@@ -22,7 +23,7 @@ public class Solution {
      * Post: Boolean stores whether a and b have the same number of char
      */
      public static boolean wordLengthMatch(String a, String b) {
-         return a.length == b.length;
+         return a.length() == b.length();
      }
 
      /*
@@ -30,11 +31,12 @@ public class Solution {
       * Post: an ArrayList<char> letters storing the characters that
       *       make up word
       */
-     public static ArrayList<char> makeLetterCollection(String word) {
-         ArrayList<char> letters = new ArrayList<char>();
+     public static ArrayList<String> makeLetterCollection(String word) {
+         ArrayList<String> letters = new ArrayList<String>();
 
-         for(char letter: word){
-             letters.add(letter);
+         for(int i = 0; i < word.length(); i++){
+             String nextLetter = word.substring(i, i+1);
+             letters.add(nextLetter);
          }
 
          return letters;
@@ -44,19 +46,16 @@ public class Solution {
      * Pre: ArrayList word and ArrayList word2, who comprise of char data
      * Post: common chars between the ArrayList will be deleted
      */
-    public static void removeCommonLetters(ArrayList<char> word1,
-                                           ArrayList<char> word2) {
+    public static void removeCommonLetters(ArrayList<String> word1,
+                                           ArrayList<String> word2) {
         // remove all char from word1, and in word2 as well if they exist
         do {
-            char letter = word1.get(0);
+            String letter = word1.get(0);
             word1.remove(0);
-            // check if the letter is in the other word
-            if (word2.contains(letter)) {
-                // letter exists, remove through a traversal
-                for(int i = 0; i < word2.size(); i++){
-                    if (letter == word2.get(i)){
-                        word2.remove(i);
-                    }
+            // if letter exists, remove through a traversal
+            for(int i = 0; i < word2.size(); i++){
+                if (letter.equalsIgnoreCase(word2.get(i))){
+                    word2.remove(i);
                 }
             }
         } while (word1.size() > 0);
@@ -65,8 +64,8 @@ public class Solution {
      * Pre: ArrayList word 1, nd ArrayList 2 storing char
      * Post: Boolean storing if both ArrayLists are emptied out
      */
-    public static boolean checkBothEmpty(ArrayList<char> word1,
-                                         ArrayList<char> word2){
+    public static boolean checkBothEmpty(ArrayList<String> word1,
+                                         ArrayList<String> word2){
         return (word1.size() == 0 && word2.size() == 0);
     }
 
@@ -78,21 +77,21 @@ public class Solution {
      */
     public static boolean isAnagram(String a, String b) {
         // if both Strings not same length, return False
-        if !(wordLengthMatch(a, b)) {
-            return False;
+        if (!(wordLengthMatch(a, b))) {
+            return false;
         } else {
             // make ArrayLists of both String, to isoloate char
-            word1 = makeLetterCollection(a);
-            word2 = makeLetterCollection(b);
+            ArrayList<String> word1 = makeLetterCollection(a);
+            ArrayList<String> word2 = makeLetterCollection(b);
 
             // remove chars held in common in both ArrayList
             removeCommonLetters(word1, word2);
 
             // make sure no letters remaining, after all common are removed
             if (checkBothEmpty(word1, word2)) {
-                return True;
+                return true;
             } else {
-                return False;
+                return false;
             }
         }
      }
