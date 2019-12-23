d376e99a4111b2b40f54d9891b04d4337e646016
= <zainr7989@gmail.com>
2019-12-23 10:24:04 -0500

    [ADD] removeCommonLetters method.

--- anagrams.java
+++ anagrams.java
@@ -40,6 +40,28 @@ public class Solution {
          return letters;
      }
 
+    /*
+     * Pre: ArrayList word and ArrayList word2, who comprise of char data
+     * Post: common chars between the ArrayList will be deleted
+     */
+    public static void removeCommonLetters(ArrayList<char> word1,
+                                           ArrayList<char> word2) {
+        // remove all char from word1, and in word2 as well if they exist
+        do {
+            char letter = word1.get(0);
+            word1.remove(0);
+            // check if the letter is in the other word
+            if (word2.contains(letter)) {
+                // letter exists, remove through a traversal
+                for(int i = 0; i < word2.size(); i++){
+                    if (letter == word2.get(i)){
+                        word2.remove(i);
+                    }
+                }
+            }
+        } while (word1.size() > 0);
+   }
+
 
     /*
      * Pre: String a and String b
